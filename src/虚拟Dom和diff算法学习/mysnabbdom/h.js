63a6e346ca2a43a6fbdba42fc5e4b6fa1227d8dc
yaoxin <yaoxin1@ztgame.com>
2021-01-25 18:58:59 +0800

    update h function init 1

---
+++ src/虚拟Dom和diff算法学习/mysnabbdom/h.js
@@ -0,0 +1,29 @@
+import vnode from './vnode'
+
+// 编写一个低配版的h函数，这个函数必须接受3个参数，缺一不可，暂不实现较复杂重载
+// 形态① h('div', {}, 'text')
+// 形态② h('div', {}, [])
+// 形态③ h('div', {}, h())
+export default function(sel, data, c) {
+  if(arguments.length !=3 ) {
+    throw new Error('必须接受3个参数，缺一不可，暂不实现较复杂重载')
+  }
+
+  console.log(sel, data, c);
+  if (typeof c == 'string' || typeof c == 'number') {
+    return vnode(sel, data, undefined, c, undefined)
+  } else if (Array.isArray(c)) {
+    let children = [];
+    for (let i = 0; i < c.length; i++) {
+      if (!(typeof c[i] == 'object'  && c[i].hasOwnProperty('sel'))) {
+        throw new Error('传入的数组中有项的结果不是h函数')
+      }
+      children.push(c[i])
+    }
+    return vnode(sel, data, children, undefined, undefined)
+  } else if (typeof c == 'object' && c.hasOwnProperty('sel')) {
+
+  } else {
+    throw new Error('传入的第三个参数类型不对')
+  }
+};
---
+++ src/虚拟Dom和diff算法学习/mysnabbdom/vnode.js
@@ -0,0 +1,6 @@
+// 将传入的参数组合成对象返回
+export default function(sel, data, children, text, elm) {
+  return {
+    sel, data, children, text, elm 
+  }
+}
