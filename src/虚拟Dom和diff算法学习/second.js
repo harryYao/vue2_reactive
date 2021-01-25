ad7e8607ad1572470a20c7ef7da6eebb9dc068b7
yaoxin <yaoxin1@ztgame.com>
2021-01-25 16:54:24 +0800

    update 学习目录更改，webpack多入口

---
+++ src/虚拟Dom和diff算法学习/second.js
@@ -0,0 +1,1 @@
+console.log('i’m second.js! ');
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        first: './src/响应式源码学习/index.js',
+        second: './src/虚拟Dom和diff算法学习/second.js'
+    },
     output: {
         publicPath: 'xuni',
-        filename: 'bundle.js'
+        filename: '[name].js'
     },
     devServer: {
         port: 8090,
