30dea0ee5f7255014fb2ab822bbe5592103c29ba
yaoxin <yaoxin1@ztgame.com>
2021-02-07 15:55:04 +0800

    打包其他资源

---
+++ src/webpack/05.打包其他资源/webpack.config.js
@@ -0,0 +1,46 @@
+/**
+ * loader  1. 下载  2. 使用
+ * plugin  1. 下载  2. 引入  3. 使用
+ */
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { Template } = require('webpack')
+
+module.exports = {
+  // 入口起点
+  entry: './src/index.js',
+  output: {
+    // 文件输入名
+    filename: 'built.js',
+    // 文件输出路径
+    // __dirname nodejs的变量，代表当前文件的目录绝对路径
+    path: resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      // 详细的loader配置
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      // 打包其他资源(除了html/js/css等之外的资源)
+      {
+        exclude: /\.(css|less|js|html)$/,
+        loader: 'file-loader', 
+        options: {
+          name: '[hash:10].[ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    // 详细的插件配置
+    // html-webpack-plugin 默认创建一个空的html, 自动引入打包输出的所有资源（js/css）
+    new HtmlWebpackPlugin({
+      // 复制模板文件，并自动引入打包输出的所有资源
+      template: './src/index.html'
+    })
+  ],
+  // 模式
+  mode: 'development'
+}
