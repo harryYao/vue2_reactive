177d6fc5072374267485cf9eec5714f9e0573e46
yaoxin <yaoxin1@ztgame.com>
2021-02-25 17:55:27 +0800

    update

--- src/webpack/12.codesplit/webpack.config.js
+++ src/webpack/12.codesplit/webpack.config.js
@@ -35,6 +35,8 @@ module.exports = {
   optimization: {
     splitChunks: {
       chunks: 'all',
+      // 以下都是默认值
+      /*
       minSize: 30 * 1024,// 分割的chunk 最小为30kb
       maxSize: 0,// 最大没有限制
       // minChunks: 1,// 要提取的chunk最少被引用1次
@@ -52,10 +54,26 @@ module.exports = {
         default: {
           // 要提取的chunk最少被引用2次
           minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
+          priority: -20,  // 优先级
+          // 如果当前要打包的模块，和之前已经被提取的模块是同一个，就会复用，而不是重新打包
+          reuseExistingChunk: true, 
         },
-      },
+      },*/
+    },
+    // 将当前模块的记录其他模块的hash单独打包为一个文件 runtime
+    // 解决： 修改a.js文件导致b文件的contenthash变化
+    runtimeChunk: {
+      name: entrypoint => `runtime-${entrypoint.name}`
+    },
+    minimizer: {
+      // 配置生产环境的压缩方案： js 和css ,需安装 terser-webpack-plugin
+      // https://webpack.docschina.org/plugins/terser-webpack-plugin/
+      // 如果你使用的是 webpack v5 或以上版本，你不需要安装这个插件。webpack v5 自带最新的 terser-webpack-plugin。如果使用 webpack v4，则必须安装 terser-webpack-plugin v4 的版本。
+      // new TerserWebpackPlugin({
+      //   cache: true, // 开启缓存
+      //   parallel: true, // 开启多进程打包
+      //   sourceMap: true // 启动source-map
+      // })
     }
   },
   mode: 'production',
