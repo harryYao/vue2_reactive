a382a897e7a1bc56324e8c0e60592158e511a2b9
harryYao <yxls0805@163.com>
2021-02-27 18:38:51 +0800

    custom call

---
+++ src/自定义工具库/call.js
@@ -0,0 +1,29 @@
+function call (fn, obj, ...args) {
+  if (obj === undefined || obj === null) {
+    obj = globalThis; // 全局对象
+  }
+  // 为obj添加临时方法
+  obj.__temp__ = fn;
+  // 调用 __temp__ 方法
+  let result = obj.__temp__(...args);
+  // 删除__temp__ 方法
+  delete obj.__temp__;
+  // 返回执行结果
+  return result
+}
+
+
+/* ---------测试--------- */
+
+
+function add(a, b) {
+  console.log(this);
+  return a + b + this.c;
+}
+let obj = {
+  c: 'objccc'
+};
+window.c = 'windowccc';
+
+console.log(call(add, obj, 1, 2));
+console.log(call(add, null, 3, 4));
