88e2b807b13033cc230ba341af109cc163572102
harryYao <yxls0805@163.com>
2021-02-27 22:07:12 +0800

    update

---
+++ src/自定义工具库/apply.js
@@ -0,0 +1,28 @@
+function apply(fn, obj, args) {
+  if (obj === undefined || obj === null) {
+    obj = globalThis; // 全局对象
+  }
+  // 为obj添加临时方法
+  obj.__temp__ = fn;
+  // 执行方法
+  let result = obj.__temp__(...args);
+  // 删除临时方法
+  delete obj.__temp__;
+
+  return result;
+}
+
+/* ---------测试--------- */
+
+
+function add(a, b) {
+  console.log(this);
+  return a + b + this.c;
+}
+let obj = {
+  c: 'objccc'
+};
+window.c = 'windowccc';
+
+console.log(apply(add, obj, [1, 2]));
+console.log(apply(add, null, [3, 4]));
