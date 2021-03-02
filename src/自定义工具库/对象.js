d1a1673f9da357143050ba9db47bc9b21b59fd0f
yaoxin <yaoxin1@ztgame.com>
2021-03-02 14:43:05 +0800

    自定义new函数

---
+++ src/自定义工具库/对象.js
@@ -0,0 +1,24 @@
+/**
+ * 自定义的new, 创建Fn构造函数的实例对象
+ * @param {*} Fn 
+ * @param  {...any} args 
+ */
+function newInstance(Fn, ...args) {
+  // 1. 创建一个新的对象
+  const obj = {};
+  // 2. 修改函数内部 this 指向新对象并执行
+  const result = Fn.call(obj, ...args);
+  // 3. 修改新对象的原型对象
+  obj.__proto__ = Fn.prototype;
+  // 4. 返回新对象
+  return obj;
+}
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+let obj = newInstance(Person, '张三', 20);
+
+console.log(obj);
