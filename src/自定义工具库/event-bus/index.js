15c54adf743f4134983d86703ee667ce0d085fa2
yaoxin <yaoxin1@ztgame.com>
2021-03-04 19:43:25 +0800

    事件总线

--- src/自定义工具库/event-bus/index.js
+++ src/自定义工具库/event-bus/index.js
@@ -5,10 +5,27 @@ const eventBus = {
 
 // 绑定事件
 eventBus.on = function(type, callback) {
-  
+  if (this.callbacks[type]) {
+    this.callbacks[type].push(callback)
+  } else {
+    this.callbacks[type] = [callback]
+  }
 }
 
 // 触发事件
 eventBus.emit = function(type, data) {
+  if (this.callbacks[type]) {
+    this.callbacks[type].forEach(fn => {
+      fn(data);
+    })
+  }
+}
 
+// 解绑事件
+eventBus.off = function(type) {
+  if (type) {
+    delete this.callbacks[type]
+  } else {
+    this.callbacks = {}
+  }
 }
