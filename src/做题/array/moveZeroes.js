a82017d7e25e7518eff17b7124d8af68d06b0852
harryYao <yxls0805@163.com>
2021-03-10 22:37:16 +0800

    update

--- src/做题/array/moveZeroes.js
+++ src/做题/array/moveZeroes.js
@@ -37,3 +37,16 @@ var moveZeroes = function(nums) {
 
 var nums = [0,1,0,3,12];
 moveZeroes(nums);
+
+// 循环前移，后为0
+var moveZeroes2 = function (nums) {
+  let k = 0;
+  for (let i = 0; i < nums.length; i++) {
+      if (nums[i]) {
+          nums[k++] = nums[i];
+      }
+  }
+  for (let i = k; i < nums.length; i++) {
+      nums[i] = 0;
+  }
+};
