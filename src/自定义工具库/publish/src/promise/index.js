a902616dd6c9049e4836494a75a5c7e2b15b97eb
yaoxin <yaoxin1@ztgame.com>
2021-03-18 17:59:33 +0800

    promise.then

--- src/自定义工具库/publish/src/promise/index.js
+++ src/自定义工具库/publish/src/promise/index.js
@@ -55,5 +55,78 @@ function Promise(excutor) {
   }
 }
 
+/**
+ * 用来指定成功/失败回调函数的方法
+ * 1> 如果当前promise 是resolved, 异步执行成功的回调函数onResolved
+ * 2> 如果当前promise 是rejected, 异步执行成功的回调函数onRejected
+ * 3> 如果当前promise 是pending, 保存回调函数，返回一个新的promise对象
+ * 它的结果状态由onResolved或者onRejected执行的结果决定
+ *  2.1). 抛出error ==> 变为rejected, 结果值为error
+ *  2.2). 返回值不是promise  ==> 变为resolved, 结果为返回值
+ *  2.1). 返回值是promise    ==> 由这个promise的决定新的promise的结果（成功/失败）
+ * @param {*} onResolved 
+ * @param {*} onRejected 
+ */
+Promise.prototype.then = function(onResolved, onRejected) {
+  const self = this;
+  onResolved = typeof onResolved === 'function' ? onResolved: value => value; // 将value向下传递
+  onRejected = typeof onRejected === 'function' ? onRejected: reason => {
+    throw reason
+  }; // 将reason向下传递
+
+
+  return new Promise((resolve, reject) => {
+
+    /**
+     * 1. 调用指定的回调函数
+     * 2. 根据回调执行结果来更新返回promise的状态
+     */
+    function handle(callback) {
+      try {
+        const result = callback(self.data)
+        if (!(result instanceof Promise)) {
+          resolve(result);
+        } else {
+          result.then(
+            value => resolve(value),
+            reason => reject(reason)
+          )
+        }
+      } catch (error) {
+        reject(error);
+      }
+    }
+
+    if (self.status === RESOLVED) {
+      setTimeout(() => {
+        handle(onRejected);
+      })
+    } else if (self.status === REJECTED) {
+      setTimeout(() => {
+        handle(onRejected)
+      })
+    } else {
+      self.callbacks.push({
+        onResolved(value) {
+          handle(onResolved)
+        },
+        onRejected(reason) {
+          handle(onRejected)
+        }
+      })
+    }
+  })
+}
+
+
+/**
+ * 用来指定失败的回调函数的方法
+ * catch是then的语法糖
+ * @param {*} onRejected 
+ * @returns 
+ */
+Promise.prototype.catch = function (onRejected) {
+  return this.then(undefined, onRejected);
+}
 
 export default Promise
