ccfed8dee2bbcc997c4c12533fefac0b4b51f2ed
harryYao <yxls0805@163.com>
2021-04-10 23:24:00 +0800

    add  log time

--- src/自定义工具库/publish/test/testlog.js
+++ src/自定义工具库/publish/test/testlog.js
@@ -2,9 +2,11 @@ const testLog = (name, fn) => {
   
   const logStyle = 'color: #A23598; font-style: italic';
   console.group(`Test Array Function: ${name}`);
+  console.time(name + '花费时间：');
   console.log('%c' + fn.toString(), logStyle);
   const result = fn();
   console.log('result:', result);
+  console.timeEnd(name + '花费时间：');
   console.groupEnd();
 }
 
